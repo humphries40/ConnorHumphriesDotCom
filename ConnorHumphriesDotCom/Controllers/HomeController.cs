ea73e2145f190ee3b89127fd3dba7900293aee1a
Connor Humphries <humphries.40@osu.edu>
2017-03-18 12:47:57 -0400

    I added a second page!
    Added page to begin placing social media links and other things to my
    presence around the interenet. Also updated homepage.

--- ConnorHumphriesDotCom/Controllers/HomeController.cs
+++ ConnorHumphriesDotCom/Controllers/HomeController.cs
@@ -13,10 +13,8 @@ namespace ConnorHumphriesDotCom.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Social()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
